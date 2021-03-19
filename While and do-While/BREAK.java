cb2d171175e3cd6c54fafdabacd8358c55b5ea61
SnakeTZZO <toithienamxinnghe@gmail.com>
2021-03-19 17:25:43 +0700

    complete while and do while in java

---
+++ While and do-While/BREAK.java
@@ -0,0 +1,9 @@
+// prints all numbers from 1 to 50 on the screen.
+public class BREAK {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (i > 50) break;
+			System.out.print(i + " ");
+		}
+	}
+}
---
+++ While and do-While/WHILE.java
@@ -0,0 +1,14 @@
+// an integer n from the user and prints all even numbers from n to 100 on the screen
+import java.util.Scanner;
+
+public class WHILE {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while(n <= 100) {
+			if (n % 2 != 0) n = n + 1; 
+			System.out.print(n + " ");
+			n += 2;
+		}
+	}
+}
