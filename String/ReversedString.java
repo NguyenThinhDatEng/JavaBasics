69c5358ee08cfc2a33c72180ba28b4ff142b2285
SnakeTZZO <toithienamxinnghe@gmail.com>
2021-03-22 16:36:46 +0700

    there are many methods is used for String in Java

---
+++ String/ReversedString.java
@@ -0,0 +1,16 @@
+// Given a string s
+// Write a program that accepts s from the user and reverses s. Display the reversed string on the screen
+import java.util.Scanner;
+
+public class ReversedString {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String str = "";
+		for (int i = s.length() - 1; i >= 0; i--)
+		{
+			str += s.charAt(i);
+		}
+		System.out.print(str);
+	}
+}
